7884e0d92f46ec5d84c909941802a8ae7954bab7
Goran Franjic <goran@proces.hr>
2024-01-18 11:26:17 +0100

    promjena id
    automatsko dodavanje id broja

--- kino.sql
+++ kino.sql
@@ -1,12 +1,12 @@
 CREATE TABLE film (
-  id INT PRIMARY KEY,
+  id INT NOT NULL PRIMARY INDETITY(1,1),
   naziv VARCHAR(255) NOT NULL,
   trajanje INT NOT NULL,
   dvorana VARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE kupac (
-  id INT PRIMARY KEY,
+  id INT NOT NULL PRIMARY INDETITY(1,1),
   ime VARCHAR(255) NOT NULL,
   prezime VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE kupac (
 );
 
 CREATE TABLE rezervacija (
-  id INT PRIMARY KEY,
+  id INT NOT NULL PRIMARY INDETITY(1,1),
   film_id INT NOT NULL,
   kupac_id INT NOT NULL,
   broj_sjedala INT NOT NULL,
