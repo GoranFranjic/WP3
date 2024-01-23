1c627d2a33b1d9a587ae4383926b496dd649661b
Goran Franjic <goran@proces.hr>
2024-01-23 20:58:52 +0100

    Inserti

--- kino.sql
+++ kino.sql
@@ -22,3 +22,25 @@ CREATE TABLE rezervacija (
   FOREIGN KEY (film_id) REFERENCES film(id),
   FOREIGN KEY (kupac_id) REFERENCES kupac(id)
 );
+
+
+
+insert into film (naziv,trajanje,dvorana) 
+values 
+('Vrisak',120,5),('Krug',120,7),('Osvetnici',150,3);
+
+
+
+insert into kupac (ime,prezime,email,broj_telefona) 
+values 
+('Tomislav','Tomić','to@fr.hr','098765432'),('Omislav','Omić','ko@fr.hr','099765432'),('Mislav','Mić','loo@fr.hr','095765432');
+
+
+insert into rezervacija (film_id,kupac_id,broj_sjedala,datum) 
+values (1,1,55,'2023-11-29 19:00:00'),
+(2,2,56,'2023-11-29 18:00:00'),(3,3,57,'2023-11-29 17:00:00');
+
+
+select * from rezervacija;
+select * from kupac;
+select * from film;
